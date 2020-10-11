f774956c7e34d8983e77f9c9282c8be201027f03
dl10yr <50223759+dl10yr@users.noreply.github.com>
2020-10-11 10:47:56 +0900

    Update firebaseClient.ts

--- firebaseClient.ts
+++ firebaseClient.ts
@@ -3,13 +3,14 @@ import 'firebase/auth';
 
 if (typeof window !== 'undefined' && !firebaseClient.apps.length) {
   const CLIENT_CONFIG = {
-    apiKey: 'YOUR_API_KEY_HERE',
-    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://YOUR_PROJECT_ID.firebaseio.com',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-    messagingSenderId: 'XXXXXXXXX',
-    appId: '1:XXXXXXXXXXXXX:web:XXXXXXXXXXXXX',
+    apiKey: "AIzaSyB1hzteIZjF3K726__bJwBaiPaZDwm_QxY",
+    authDomain: "watcher-tube.firebaseapp.com",
+    databaseURL: "https://watcher-tube.firebaseio.com",
+    projectId: "watcher-tube",
+    storageBucket: "watcher-tube.appspot.com",
+    messagingSenderId: "302546742936",
+    appId: "1:302546742936:web:4fab1d136375594d11aaed",
+    measurementId: "G-N57MELFS3Y"
   };
 
   firebaseClient.initializeApp(CLIENT_CONFIG);
