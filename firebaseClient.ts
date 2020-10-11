eccfe828c53fba4b6f326fbb9ef006ceedd3aa0e
dl10yr <50223759+dl10yr@users.noreply.github.com>
2020-10-11 11:10:03 +0900

    Update firebaseClient.ts

--- firebaseClient.ts
+++ firebaseClient.ts
@@ -3,7 +3,14 @@ import 'firebase/auth';
 
 if (typeof window !== 'undefined' && !firebaseClient.apps.length) {
   const CLIENT_CONFIG = {
-
+    apiKey: "AIzaSyB1hzteIZjF3K726__bJwBaiPaZDwm_QxY",
+    authDomain: "watcher-tube.firebaseapp.com",
+    databaseURL: "https://watcher-tube.firebaseio.com",
+    projectId: "watcher-tube",
+    storageBucket: "watcher-tube.appspot.com",
+    messagingSenderId: "302546742936",
+    appId: "1:302546742936:web:4fab1d136375594d11aaed",
+    measurementId: "G-N57MELFS3Y"
   };
 
   firebaseClient.initializeApp(CLIENT_CONFIG);
